1b05698280af06e3ed0b20f097b2f3122228e528
Timothy Ng <tdng@umass.edu>
2016-03-05 23:34:27 -0500

    Added a move to target script to be used in overworld

---
+++ Peace-Superheroes/Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class MoveToTarget : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+	}
+
+    public void ChangeTarget(Transform newtransform)
+    {
+        Assert.IsNotNull(newtransform, "Cannot change target to a null transform");
+
+        target = newtransform;
+    }
+}
