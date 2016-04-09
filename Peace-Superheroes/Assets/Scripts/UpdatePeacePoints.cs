5aa9f0fbdb6c1cc513483d1d83bcb1d4473a9137
Timothy Ng <tdng@umass.edu>
2016-04-08 21:31:29 -0400

    timer implemented

---
+++ Peace-Superheroes/Assets/Scripts/UpdatePeacePoints.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+using UnityEngine.UI;
+
+public class UpdatePeacePoints : MonoBehaviour {
+
+    public Flowchart flowchart;
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text = "PeacePoints: " + flowchart.GetVariable<IntegerVariable>("PeacePoints").value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "PeacePoints: " + flowchart.GetVariable<IntegerVariable>("PeacePoints").value;
+    }
+}
